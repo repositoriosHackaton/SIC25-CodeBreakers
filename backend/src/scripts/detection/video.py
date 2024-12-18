460688b70e894d26c3994401310aaaf7007e394d
JoshuaCarrera <cubyt3@gmail.com>
2024-12-18 13:22:11 -0400

    Pruebas detections

---
+++ backend/src/scripts/detection/video.py
@@ -0,0 +1,91 @@
+import numpy as np
+import cv2
+
+from PIL import Image, ImageDraw, ImageOps
+
+
+
+
+# Función para unir puntos cercanos en un contorno
+def simplify_contour(contour, distance_threshold):
+    simplified_contour = [contour[0][0]]  # Inicia con el primer punto
+    for point in contour[1:]:
+        last_point = simplified_contour[-1]
+        # Calcular la distancia euclidiana entre el punto actual y el último en la lista
+        distance = np.linalg.norm(point[0] - last_point)
+        if distance > distance_threshold:
+            simplified_contour.append(point[0])  # Agregar si la distancia es mayor al umbral
+    return np.array(simplified_contour, dtype="int32")
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+while True:
+    ret, image= cap.read()
+    # Convertir a escala de grises
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Aplicar un filtro para reducir el ruido
+    # blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    blurred = cv2.bilateralFilter(gray, 9, 75, 75)
+
+
+    # Detectar bordes con Canny
+    low_threshold = 300  # Ajustar estos valores según el contraste de la imagen
+    high_threshold = 25
+    edges = cv2.Canny(blurred, low_threshold, high_threshold)
+
+
+    # Dilate the image
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))  # Tamaño del kernel ajustable
+    dilataded = cv2.dilate(edges, kernel, iterations=1)
+    dilataded = cv2.morphologyEx(dilataded, cv2.MORPH_CLOSE, kernel)
+
+
+    # Mostrar la imagen de bordes
+    cv2.imshow("Bordes Detectados (Canny)", dilataded)
+
+    # Encontrar contornos
+    contours, _ = cv2.findContours(dilataded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    if contours:
+        # Buscar el contorno más grande (suponiendo que es el billete)
+        largest_contour = max(contours, key=cv2.contourArea)
+
+        peri = cv2.arcLength(largest_contour, True)
+        epsilon = 0.02 * peri
+        approx = cv2.approxPolyDP(largest_contour, epsilon, True)
+
+        #distance_threshold = 250  # Umbral de distancia
+        #simplified_contour = simplify_contour(approx, distance_threshold)
+        #print(len(approx), "->", len(simplified_contour))
+
+        # Dibujar el contorno detectado
+        result = image.copy()
+        cv2.drawContours(result, [approx], -1, (0, 255, 0), 2)
+        cv2.imshow('result', result)
+
+
+    """
+    # Mostrar la imagen con el contorno detectado
+    cv2.imshow('Webcam', image)
+    cv2.imshow('dilataded', dilataded)
+
+    #color_coverted = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 
+    PIL_org_image = Image.fromarray(image) 
+    PIL_bin_image = Image.fromarray(dilataded).convert('L')
+    binary_image = PIL_bin_image.point(lambda p: p > 128 and 255)
+    bbox = binary_image.getbbox()  # (x_min, y_min, x_max, y_max)
+
+    draw = ImageDraw.Draw(PIL_org_image)
+    draw.rectangle(bbox, outline="green", width=10)
+
+    cv2.imshow("bounding box", np.array(PIL_org_image))
+    """
+
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
