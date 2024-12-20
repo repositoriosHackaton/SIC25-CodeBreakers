acaea19be6a119ff8e2744588badf0bc6b7cd7a2
Jsssus <jesusrhivan@gmail.com>
2024-12-20 00:39:03 -0400

    agregando el primer modelo que probré (el fallido) Quantity Detection

--- backend/Dollar_Bill_Detection/Main.py
+++ backend/Dollar_Bill_Detection/Main.py
@@ -1,12 +1,13 @@
 from ultralytics import YOLO
 import torch.multiprocessing as mp
 
+
 def train_model():
     # Cargar el modelo YOLO preentrenado
     model = YOLO("yolov8n.yaml") 
     
     # Entrenar el modelo
-    results = model.train(data=r'C:\Users\jesus\Desktop\PRUEBA_TENSOR\Dollar_Bill_Detection\data.yaml', epochs = 1)
+    results = model.train(data='backend/Dollar_Bill_Detection/data.yaml', epochs = 1)
     print(results)
 
 if __name__ == "__main__":
---
+++ backend/modelo 1 quantity detection.v1i.yolov8-obb/README.dataset.txt
@@ -0,0 +1,6 @@
+# quantity detection > 2023-07-01 12:56pm
+https://universe.roboflow.com/ic4u-saornek/quantity-detection
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
