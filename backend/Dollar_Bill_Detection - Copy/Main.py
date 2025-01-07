15351b4fe1f2dd06328af0fae5307cdb1b63ff91
Jsssus <jesusrhivan@gmail.com>
2025-01-07 03:22:44 -0400

    eliminando carpetas, creando una copia del primer modelo para actualizar el data set con el billete de 100. Modelo con billete de 100 creado y funcionando

---
+++ backend/Dollar_Bill_Detection - Copy/Main.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+import torch.multiprocessing as mp
+
+#Agregando una ruta dinámica para los datasets
+from ultralytics import settings
+settings.update(datasets_dir = '.')
+
+def train_model():
+    # Cargar el modelo YOLO preentrenado
+    model = YOLO("yolov8n.yaml") 
+    
+    # Entrenar el modelo
+    results = model.train(data=r'C:\Users\jesus\Desktop\Clones\cash_reader\backend\Dollar_Bill_Detection - Copy\data.yaml', epochs = 100)
+    print(results)
+
+if __name__ == "__main__":
+    mp.freeze_support()  #Evitar un error con Windows
+    train_model()  #Llamar a la función que entrena el modelo
