7b13f3de6faa8c08a98066f09e899c9bebb7f86e
Jsssus <jesusrhivan@gmail.com>
2025-01-13 00:11:20 -0400

    agregando script de rotacion rotation.py

---
+++ backend/src/scripts/preprocessing/rotation.py
@@ -0,0 +1,35 @@
+import os
+import random
+from PIL import Image
+
+def rotate_image(image_path, angle):
+    with Image.open(image_path) as img:
+        return img.rotate(angle, expand=True)
+
+def process_images(input_dir, output_dir, ranges):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    all_images = [f for f in os.listdir(input_dir) if f.endswith((".jpg", ".jpeg", ".png"))]
+    num_images_per_range = len(all_images) // 4  #dividir en partes iguales de 25%
+
+    for idx, angle_range in enumerate(ranges):
+        selected_images = random.sample(all_images, num_images_per_range)
+        all_images = [img for img in all_images if img not in selected_images]
+
+        for image_name in selected_images:
+            image_path = os.path.join(input_dir, image_name)
+            angle = random.uniform(*angle_range)
+            rotated_image = rotate_image(image_path, angle)
+
+            output_image_path = os.path.join(output_dir, image_name)
+            rotated_image.save(output_image_path)
+
+    print("Procesamiento completado.")
+
+# Configuración
+input_dir = r'C:\Users\jesus\Desktop\Clones\cash_reader\backend\src\data\img-data-vef\processed\50\1\back'
+output_dir = r'C:\Users\jesus\Desktop\Clones\cash_reader\backend\src\data\img-data-vef\processed\50\1\back2'
+ranges = [(-180, -170), (-180,-170)]  # Rango de ángulos en grados, son dos angulos, por lo tanto será la mitad del 100%
+
+process_images(input_dir, output_dir, ranges)
