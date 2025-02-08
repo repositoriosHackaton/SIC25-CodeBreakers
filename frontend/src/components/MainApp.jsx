d24bbd62840c90904f26c11db1e8ce3a4d78373b
JoelEscobar13 <joelescobar216@gmail.com>
2025-02-08 11:20:32 -0400

    Activacion3
    Activando el VoiceInterface por tercera vez

--- frontend/src/components/MainApp.jsx
+++ frontend/src/components/MainApp.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 import Header from "./Header";
 import Camera from "./Camera";
 import VoiceInterface from "./VoiceInterface";
 import "./MainApp.css";
 
-const App = () => {
+const MainApp = () => {
+    const cameraRef = useRef(null); // Referencia para acceder a la cámara
+
     return (
         <div className="container">
             <header className="header">
                 <Header />
             </header>
-            <Camera />
+
+            {/* Montar la cámara con referencia */}
+            <Camera ref={cameraRef} />
+
+            {/* Montar la interfaz de voz (escucha comandos siempre) */}
+            <VoiceInterface cameraRef={cameraRef} />
         </div>
     );
 };
