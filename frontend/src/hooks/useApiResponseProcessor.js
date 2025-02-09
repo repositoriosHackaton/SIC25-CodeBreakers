2d3b86f25b48666fa60e5156124952e8743870ec
Fran2310 <106452242+Fran2310@users.noreply.github.com>
2025-02-09 19:44:06 -0400

    Add bolivares a labelMap

--- frontend/src/hooks/useApiResponseProcessor.js
+++ frontend/src/hooks/useApiResponseProcessor.js
@@ -27,7 +27,7 @@ const useApiResponseProcessor = (narrate) => {
             }
 
             const labelMap = {
-                "fifty-back": "cincuenta dólares",
+                "fifty-back": "cincuenta dólares", //USD
                 "fifty-front": "cincuenta dólares",
                 "five-back": "cinco dólares",
                 "five-front": "cinco dólares",
@@ -39,6 +39,18 @@ const useApiResponseProcessor = (narrate) => {
                 "twenty-front": "veinte dólares",
                 "one_hundred-back": "cien dólares",
                 "one_hundred-front": "cien dólares",
+                "fifty-back-vef": "cincuenta bolívares", //VEF
+                "fifty-front-vef": "cincuenta bolívares",
+                "five-back-vef": "cinco bolívares",
+                "five-front-vef": "cinco bolívares",
+                "ten-back-vef": "diez bolívares",
+                "ten-front-vef": "diez bolívares",
+                "twenty-back-vef": "veinte bolívares",
+                "twenty-front-vef": "veinte bolívares",
+                "one_hundred-back-vef": "cien bolívares",
+                "one_hundred-front-vef": "cien bolívares",
+                "two_hundred-back-vef": "dos cientos bolívares",
+                "two_hundred-front-vef": "dos cientos bolívares",
             };
 
             const validDetections = apiResponse.detections.filter(
