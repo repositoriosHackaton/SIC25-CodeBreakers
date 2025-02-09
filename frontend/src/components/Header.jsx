aa3abd5c12d15ee3842272d7514d1935c5ff7df8
Fran2310 <106452242+Fran2310@users.noreply.github.com>
2025-02-09 05:12:51 -0400

    Debug narracion doble en info

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,6 @@ import useNarrator from "../hooks/useNarrator";
 import "./Header.css";
 
 const Header = () => {
-    const [isActive, setIsActive] = useState(false);
     const [narration, setNarration] = useState("");
 
     // Función para limpiar la narración después de hablar
@@ -15,21 +14,14 @@ const Header = () => {
     useNarrator(narration, handleNarrationComplete);
 
     const handleClick = () => {
-        setIsActive(true);
-
         // Iniciar narración
         setNarration(
             "Ayuda para los comandos de voz. Puede pronunciar Tomar Foto para evaluar el valor de un billete."
         );
-
-        // Remueve la clase "active" después de un corto tiempo, esto es para la animacion del css
-        setTimeout(() => {
-            setIsActive(false);
-        }, 200);
     };
 
     return (
-        <button className={`header-button ${isActive ? "active" : ""}`} onClick={handleClick}>
+        <button className={`header-button`} onClick={handleClick}>
             <div className="logo-container">
                 <img src="/favicon.svg" alt="Logo de la App" className="logo" />
                 <h1 className="app-name">Cash Reader</h1>
