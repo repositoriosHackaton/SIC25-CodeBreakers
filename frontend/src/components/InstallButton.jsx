8d48bfac42f045f84470211ace255d0df730f01f
Fran2310 <106452242+Fran2310@users.noreply.github.com>
2025-02-09 04:17:02 -0400

    Refact component InstallButton
    Separando estilos de componente y deshabilitando mensajes de consola

--- frontend/src/components/InstallButton.jsx
+++ frontend/src/components/InstallButton.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./InstallButton.css";
 
 const InstallButton = () => {
     const [installEvent, setInstallEvent] = useState(null);
@@ -19,6 +20,7 @@ const InstallButton = () => {
     const handleInstallClick = () => {
         if (installEvent) {
             installEvent.prompt();
+            /*
             installEvent.userChoice.then((choiceResult) => {
                 if (choiceResult.outcome === "accepted") {
                     console.log("El usuario aceptó la instalación.");
@@ -26,25 +28,14 @@ const InstallButton = () => {
                     console.log("El usuario rechazó la instalación.");
                 }
             });
+            */
         } else {
             alert("La instalación no está disponible en este momento.");
         }
     };
 
     return (
-        <button
-            onClick={handleInstallClick}
-            style={{
-                padding: "0.5rem 1rem",
-                fontSize: "1.5rem",
-                backgroundColor: "#28a745",
-                color: "#f8f9fa",
-                border: "none",
-                borderRadius: ".5rem",
-                cursor: "pointer",
-                boxShadow: "0 6px 8px var(--color-base-dark-shadow)",
-            }}
-        >
+        <button className="install-button" onClick={handleInstallClick}>
             Instalar App
         </button>
     );
