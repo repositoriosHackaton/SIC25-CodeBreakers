9ac7fe4ffec74710ed389f52bf9d5aa3817401ca
JoshuaCarrera <106785747+JoshuaCarrera@users.noreply.github.com>
2025-03-01 21:52:53 -0400

    Actualizar protocolo Camera.jsx

--- frontend/src/components/Camera.jsx
+++ frontend/src/components/Camera.jsx
@@ -181,7 +181,7 @@ const Camera = () => {
 
                 // Seleccionar el endpoint según el modelo actual
                 const endpoint = toggleModel ? "vef" : "usd";
-                const url = `https://192.168.1.35:5000/detection/${endpoint}`;
+                const url = `http://192.168.1.35:5000/detection/${endpoint}`;
 
                 const response = await axios.post(url, formData, {
                     headers: { "Content-Type": "multipart/form-data" },
